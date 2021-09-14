84be682afdb21e72e0af7104bfd634549c8cb670
AndyMc3000 <andypatmcd@gmail.com>
2021-09-14 18:53:20 +0000

    Render form in checkout.html using crispy-forms

--- lawngenie/settings.py
+++ lawngenie/settings.py
@@ -46,6 +46,9 @@ INSTALLED_APPS = [
     'products',
     'cart',
     'checkout',
+
+    # Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -74,13 +77,20 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # Required by django and django-allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'cart.contexts.cart_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Copied from django-allauth documentation - django-allauth.readthedocs.io
 AUTHENTICATION_BACKENDS = [
