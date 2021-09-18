b9ab71e2cf3039f43debcffebac408f2e32e5098
AndyMc3000 <andypatmcd@gmail.com>
2021-09-18 20:07:34 +0000

    Add real site Email address and functionality in settings.py

--- lawngenie/settings.py
+++ lawngenie/settings.py
@@ -108,8 +108,6 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1  # Required by django-allauth
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # Required to temporaily log emails to the console
-
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'  # Allow authentication by username or email
 ACCOUNT_EMAIL_REQUIRED = True  # Ensures email is a requirement for account registration
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'  # Ensures Email verification is required for account setup
@@ -184,7 +182,7 @@ if 'USE_AWS' in os.environ:
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
         'CacheControl': 'max-age=94608000',
     }
-    
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'lawngenie'
     AWS_S3_REGION_NAME = 'eu-west-1'
@@ -210,8 +208,18 @@ STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
-DEFAULT_FROM_EMAIL = 'team@lawngenie.com'
 
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
