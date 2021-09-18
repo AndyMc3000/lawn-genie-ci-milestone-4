1cedfcac16f50fe21729493a35fb04c152d8275b
AndyMc3000 <andypatmcd@gmail.com>
2021-09-18 11:30:20 +0000

    Update ALLOWED_HOSTS in settings.py

--- lawngenie/settings.py
+++ lawngenie/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ckz8780-boutique-ado.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['lawn-genie.herokuapp.com/', 'localhost']
 
 
 # Application definition
