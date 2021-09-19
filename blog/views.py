3b9d81e9da60d41dc3bfe37e095db85499dc386e
AndyMc3000 <andypatmcd@gmail.com>
2021-09-19 14:10:21 +0000

    Add Blog model to the Admin portal, and add Blog views, url patterns, and templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.views import generic
+
+from .models import Post
 
 # Create your views here.
+
+
+class PostList(generic.ListView):
+    """ A view to return a list of all blog posts """
+
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'blog/blog.html'
+
+
+class PostDetail(generic.DetailView):
+    """ A view to return the detail of a blog post """
+
+    model = Post
+    template_name = 'blog/post_detail.html'
